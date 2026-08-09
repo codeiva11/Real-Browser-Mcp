ae9d0d35cdcb7f4599865c71051afbbae5582a1d
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2026-08-09 23:48:10 +0530

    Update test.mjs

--- test/test.mjs
+++ test/test.mjs
@@ -128,7 +128,7 @@ test('Recaptcha V3 Score', async () => {
 
     // 4. Now click the button
     await page.realClick("button")
-    await new Promise(r => setTimeout(r, 5800));
+    await new Promise(r => setTimeout(r, 6000));
 
     const score = await page.evaluate(() => {
         return document.querySelector('big').textContent.replace(/[^0-9.]/g, '')
